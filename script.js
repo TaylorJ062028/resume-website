41d60c64417cf281df7b3882e31cd7b3bd0e32f3
TaylorJ062028 <tjj5499@psu.edu>
2026-04-21 13:52:31 -0400

    Add toggle button for dark mode and download alert

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("toggleBtn");
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+
+function downloadPDF() {
+  alert("Download feature coming soon!");
+}
